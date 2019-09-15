9d41e9489cc2e53249fc829a3c7e1ac5034c0762
Lizi (遭重后) <liritian1994@gmail.com>
2019-09-14 22:45:59 -0500

    Initial Commit

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import nba from 'nba';
+import {Profile} from "./Profile";
+
+class Main extends Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
+    }
+
+    componentDidMount() {
+        console.log('nba.stats --', nba.stats);
+
+        nba.stats.playerInfo({ PlayerID: this.state.playerId })
+            .then((info) => {
+                console.log(info);
+                const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log('playerInfo', playerInfo);
+                this.setState({ playerInfo });
+            })
+            .catch((e) => console.log(e))
+
+    }
+
+    render() {
+        console.log('player --', this.state.playerInfo);
+        return (
+            <div className="main">
+                <Profile playerInfo = {this.state.playerInfo}/>
+                this is player
+            </div>
+        );
+    }
+}
+
+export default Main;
